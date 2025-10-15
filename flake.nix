5abe64b40519e24a6fe1cf3001e0ae6b6d6d1077
z4roc <arthuraktamirov@gmail.com>
2025-10-15 15:20:37 +0200

    add input

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.zaroc = import ./home/zaroc.nix;
           }
         ];
@@ -42,6 +43,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.zaroc = import ./home/zaroc.nix;
           }
         ];
@@ -57,6 +59,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.zaroc = import ./home/server.nix;
           }
         ];
@@ -71,6 +74,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.zaroc = import ./home/zaroc.nix;
           }
         ];
